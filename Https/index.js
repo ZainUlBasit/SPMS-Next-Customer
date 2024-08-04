0c16860ce6738bb0149d0687520569440fbbecf7
ZainUlBasit <zainulbasit486@gmail.com>
2024-08-05 03:06:12 +0500

    changes

--- Https/index.js
+++ Https/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://spms-next-customer.vercel.app/api",
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
@@ -13,7 +13,7 @@ export const api = axios.create({
 });
 
 export const apiForImage = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://spms-next-customer.vercel.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
